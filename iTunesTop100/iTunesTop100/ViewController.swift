bdd8f0bc0401842f61622050eb367267b20a38e5
Rodrigo Candido <rodrigoipu@hotmail.com>
2021-04-08 17:19:55 +0200

    Initial commit - Create iOS empty project and clean up for initial commit Update ignore file to adapt project requirements

---
+++ iTunesTop100/iTunesTop100/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesTop100
+//
+//  Created by Rodrigo  Candido on 8/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
